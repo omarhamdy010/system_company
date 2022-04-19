49d5bbabd2ecdc41fd0574ae52712d460c405489
omarhamdy010 <hamdyomar065@gmail.com>
2022-04-19 11:19:04 +0200

    add function calc

--- app/Http/Controllers/Dashboard/PageUserController.php
+++ app/Http/Controllers/Dashboard/PageUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\PageUser;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
@@ -37,6 +38,7 @@ class PageUserController extends Controller
         ];
 
         $pag->update($pageuser);
+
 
 
         return response()->json(
@@ -135,9 +137,15 @@ class PageUserController extends Controller
         return redirect()->back();
     }
 
+    //this function to return time by minutes
     public function calc(){
-        $x = PageUser::where('user_id',auth()->user()->id)->get();
-        dd($x);
+        $pageUser = PageUser::where('user_id',auth()->user()->id)->first();
+        $now = Carbon::now();
+        $created_at = Carbon::parse($pageUser['created_at']);
+//        $diffHuman = $created_at->diffForHumans($now);  // 3 Months ago
+//        $diffHours = $created_at->diffInHours($now);  // 3
+        $diffMinutes = $created_at->diffInMinutes($now);  // 180
+        return $diffMinutes ;
     }
 
 }
