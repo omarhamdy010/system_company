6cafa41f422c253c4ba123bcc38779c7c6720811
omarhamdy010 <hamdyomar065@gmail.com>
2022-07-17 01:27:22 +0200

    'api'

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/api_register', [App\Http\Controllers\API\AuthController::class, 'api_register'])->name('api_register');
+Route::post('/api_login', [App\Http\Controllers\API\AuthController::class, 'api_login'])->name('api_login');
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/api_logout', [App\Http\Controllers\API\AuthController::class, 'api_logout']);
